30548de742d4b87074ee25718d8ff2949300daab
codingrankschool <jiraja2915@gmail.com>
2026-06-12 06:38:50 +0530

    Added content for sprint 2 day 4

---
+++ Unit 1/Sprint 2/Day 4/Javascript/variables.js
@@ -0,0 +1,24 @@
+
+
+// Variables: Like a container which hold data and we assign name so that we can use or change later
+
+
+// How create variable:
+
+// syntax
+// var variable_name
+
+
+// Declaration of variable(creating the variable)
+
+var helloJavascript = "Welcome to Javascript"
+
+var age = 45;
+
+
+// we have console.log()  it is like a tool to print variables data on console:
+
+// syntax
+
+console.log(helloJavascript)
+console.log(age)
